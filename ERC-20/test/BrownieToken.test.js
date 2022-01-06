ea4f382266f56d94ea16035e33849e537aa9c3eb
spandan114 <spandanj685@gmail.com>
2022-01-07 00:26:48 +0530

    test cases for total supply

--- ERC-20/test/BrownieToken.test.js
+++ ERC-20/test/BrownieToken.test.js
@@ -1,14 +1,14 @@
 
 const BrownieToken = artifacts.require("BrownieToken");
  
-contract('BrownieToken', (accounts) => {
+contract('BrownieToken', async(accounts) => {
 //   it("should return the list of accounts", async ()=> {
 //     console.log(accounts);
 //   });
-it("Total supply should match with 100000", async ()=> {
-    const instance = await BrownieToken.deployed();
-    const supply = await instance.totalSupply_();
+const instance = await BrownieToken.deployed();
 
+it("Total supply should match with 100000", async ()=> {
+    const supply = await instance.totalSupply_();
     assert.equal(supply, 100000,"Total supply must 100000");
   });
 });
